86453283baa74d9d9201bddc6935d35f2b144a28
dwivedinisha <dwivedinisha975@gmail.com>
2025-08-16 16:51:29 +0530

    button styling changed

--- index.js
+++ index.js
@@ -3,21 +3,7 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-// let num = document.getElementById("input-text").valueAsNumber;
 
-// // let num = document.querySelector("input").valueAsNumber;
-// const convert_btn = document.getElementById("convert-btn");
-// let length_convert = document.getElementById("length");
-// let volume_convert = document.getElementById("volume");
-// let mass_convert = document.getElementById("mass");
-// // console.log(num)
-// let value_in_feet = (num*3.281).toFixed(3);
-// let value_in_meter = (num/3.281).toFixed(3);
-// let value_in_gallon = (num*0.264).toFixed(3);
-// let value_in_litre = (num/0.264).toFixed(3);
-// let value_in_pound = (num*2.204).toFixed(3);
-// let value_in_kilogram = (num/2.204).toFixed(3);
-// // console.log(value_in_feet)
 
 function convert(){
     let num = document.getElementById("input-text").valueAsNumber;
